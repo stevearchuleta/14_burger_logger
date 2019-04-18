b8de8613156706498c89dd3130424ba4c99483e8
Steve Archuleta <stevearchuleta@me.com>
2019-04-18 08:42:21 -0700

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Meese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bobbi Burger');
+INSERT INTO burgers (burger_name) VALUES ('Sylvester Burger');
+INSERT INTO burgers (burger_name) VALUES ('Manson Burger');
+INSERT INTO burgers (burger_name) VALUES ('Biden Burger');
+INSERT INTO burgers (burger_name) VALUES ('Owen Burger');
