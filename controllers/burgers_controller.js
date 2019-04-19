47b85dc01f1adfd9db111eb45b924d047e9222e9
Steve Archuleta <stevearchuleta@me.com>
2019-04-18 18:15:25 -0700

    working with Sung's help

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -16,43 +16,43 @@ router.get("/", function(req, res) {
   });
 });
 
-router.post("/api/burgers", function(req, res) {
-  cat.create([
-    "name", "devoured"
+router.post("/burgers", function(req, res) {
+  burger.create([
+    "burger_name", "devoured"
   ], [
-    req.body.name, req.body.devoured
+    req.body.burger_name, req.body.devoured
   ], function(result) {
     // Send back the ID of the new quote
-    res.json({ id: result.insertId });
+    res.redirect('/');
   });
 });
 
-router.put("/api/burgers/:id", function(req, res) {
+router.put("/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
-  cat.update({
-    sleepy: req.body.devoured
+  burger.update({
+    devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
     } else {
-      res.status(200).end();
+      res.redirect('/');
     }
   });
 });
 
-router.delete("/api/burgers/:id", function(req, res) {
+router.delete("/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
-  cat.delete(condition, function(result) {
+  burger.delete(condition, function(result) {
     if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
     } else {
-      res.status(200).end();
+      res.redirect('/');
     }
   });
 });
--- server.js
+++ server.js
@@ -4,9 +4,11 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
+var methodOverride = require("method-override");
+
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
-
+app.use(methodOverride("_method"));
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
